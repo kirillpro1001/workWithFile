5a1e6842697f18a3910c6a591ff273dad0b8e5fd
Kirill Bystrov <kir9aza@gmail.com>
2022-11-08 22:25:03 +0300

    write to file

--- files.php
+++ files.php
@@ -1,8 +1,7 @@
 <?php 
-$file = fopen (__DIR__.'/file.txt','r');
-while (!feof($file)) {
-	echo fgets ($file);
-	echo '<br>';
+$file = fopen (__DIR__.'/file.txt','w');
+for ($i = 1; $i <= 100; $i++) {
+	fputs($file,$i.PHP_EOL);
 }
 fclose($file);
 ?>
